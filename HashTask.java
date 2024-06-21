29dd35a85bba3dd389bc6ee773bdb31e6b610e10
Aryna Reutska <133893032+xrendezvous@users.noreply.github.com>
2024-06-21 19:43:39 +0300

    updated hashtask.java

--- HashTask.java
+++ HashTask.java
@@ -1,63 +1,61 @@
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
 import java.security.SecureRandom;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class HashTask {
 
     public static void main(String[] args) {
         try {
-            String data = "exampleData";
+            String data = "someExampleDataToHash";
 
-            String md5Hash = generateHash("MD5", data);
-            String sha1Hash = generateHash("SHA-1", data);
-            String sha256Hash = generateHash("SHA-256", data);
+            String md5Hash = hashGen("MD5", data);
+            String sha1Hash = hashGen("SHA-1", data);
+            String sha256Hash = hashGen("SHA-256", data);
 
             System.out.println("MD5 Hash: " + md5Hash);
             System.out.println("SHA-1 Hash: " + sha1Hash);
             System.out.println("SHA-256 Hash: " + sha256Hash);
 
-            byte[] random1 = generateSecureRandom("SHA1PRNG");
-            byte[] random2 = generateSecureRandom("Windows-PRNG");
-            byte[] random3 = generateSecureRandom("SHA1PRNG");
+            byte[] random1 = secureRandGen("SHA1PRNG");
+            byte[] random2 = secureRandGen("Windows-PRNG");
+            byte[] random3 = secureRandGen("SHA1PRNG");
 
             System.out.println("SecureRandom SHA1PRNG: " + bytesToHex(random1));
             System.out.println("SecureRandom Windows-PRNG: " + bytesToHex(random2));
             System.out.println("SecureRandom SHA1PRNG (again): " + bytesToHex(random3));
 
-            MyCorrectClass correctObject1 = new MyCorrectClass(1, "Object1");
-            MyCorrectClass correctObject2 = new MyCorrectClass(1, "Object1");
+            correctFunctions correctObject1 = new correctFunctions(1, "Object1");
+            correctFunctions correctObject2 = new correctFunctions(1, "Object1");
 
-            MyIncorrectClass incorrectObject1 = new MyIncorrectClass(1, "Object1");
-            MyIncorrectClass incorrectObject2 = new MyIncorrectClass(2, "Object2");
+            incorrectFunctions incorrectObject1 = new incorrectFunctions(1, "Object1");
+            incorrectFunctions incorrectObject2 = new incorrectFunctions(2, "Object2");
 
-            Map<MyCorrectClass, String> correctMap = new HashMap<>();
+            Map<correctFunctions, String> correctMap = new HashMap<>();
             correctMap.put(correctObject1, "CorrectObject1");
             correctMap.put(correctObject2, "CorrectObject2");
 
-            Map<MyIncorrectClass, String> incorrectMap = new HashMap<>();
+            Map<incorrectFunctions, String> incorrectMap = new HashMap<>();
             incorrectMap.put(incorrectObject1, "IncorrectObject1");
             incorrectMap.put(incorrectObject2, "IncorrectObject2");
 
             System.out.println("Correct Map Size: " + correctMap.size());
             System.out.println("Incorrect Map Size: " + incorrectMap.size());
 
-            saveHashesToFile(md5Hash, sha1Hash, sha256Hash, random1, random2, random3);
+            hashToFile(md5Hash, sha1Hash, sha256Hash, random1, random2, random3);
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-    public static String generateHash(String algorithm, String data) throws NoSuchAlgorithmException {
+    public static String hashGen(String algorithm, String data) throws NoSuchAlgorithmException {
         MessageDigest messageDigest = MessageDigest.getInstance(algorithm);
         byte[] hashBytes = messageDigest.digest(data.getBytes());
         return bytesToHex(hashBytes);
     }
 
-    public static byte[] generateSecureRandom(String algorithm) throws NoSuchAlgorithmException {
+    public static byte[] secureRandGen(String algorithm) throws NoSuchAlgorithmException {
         SecureRandom secureRandom = SecureRandom.getInstance(algorithm);
         byte[] randomBytes = new byte[16];
         secureRandom.nextBytes(randomBytes);
@@ -72,7 +70,7 @@ public class HashTask {
         return sb.toString();
     }
 
-    public static void saveHashesToFile(String md5, String sha1, String sha256, byte[] random1, byte[] random2, byte[] random3) throws IOException {
+    public static void hashToFile(String md5, String sha1, String sha256, byte[] random1, byte[] random2, byte[] random3) throws IOException {
         try (FileWriter writer = new FileWriter("hashes.txt")) {
             writer.write("MD5 Hash: " + md5 + "\n");
             writer.write("SHA-1 Hash: " + sha1 + "\n");
@@ -84,11 +82,11 @@ public class HashTask {
     }
 }
 
-class MyCorrectClass {
+class correctFunctions {
     private int id;
     private String name;
 
-    public MyCorrectClass(int id, String name) {
+    public correctFunctions(int id, String name) {
         this.id = id;
         this.name = name;
     }
@@ -97,7 +95,7 @@ class MyCorrectClass {
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
-        MyCorrectClass that = (MyCorrectClass) o;
+        correctFunctions that = (correctFunctions) o;
         return id == that.id && name.equals(that.name);
     }
 
@@ -109,11 +107,11 @@ class MyCorrectClass {
     }
 }
 
-class MyIncorrectClass {
+class incorrectFunctions {
     private int id;
     private String name;
 
-    public MyIncorrectClass(int id, String name) {
+    public incorrectFunctions(int id, String name) {
         this.id = id;
         this.name = name;
     }
